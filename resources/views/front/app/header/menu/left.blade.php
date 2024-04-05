7059875e9c35ed12fee9cee0711f10578d1e2724
Resul <parahadovrp@gmail.com>
2024-04-05 13:35:26 +0500

    icons added

--- resources/views/front/app/header/menu/left.blade.php
+++ resources/views/front/app/header/menu/left.blade.php
@@ -5,17 +5,19 @@
     .cart-count {
         position: absolute;
         right: -11px;
-        top: -8px;
+        top: -5px;
         border-radius: 50%;
         height: 15px;
         width: 15px;
         vertical-align: middle;
-        text-align: center;
-        margin: auto;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         background-color: var(--insur-primary);
-        padding: 2px;
         font-weight: bold;
-        font-size: 12px;
+        font-size: 10px;
+        line-height: 10px;
         color: var(--insur-white);
         font-family: 'Roboto-Regular', serif !important;
     }
@@ -95,22 +97,30 @@
                 <li class="{{ Route::currentRouteName() == 'contact.index' ? 'current ' : '' }}">
                     <a href="{{ route('contact.index') }}">@lang('transFront.contact')</a>
                 </li>
-                <li class="{{ Route::currentRouteName() == 'cart' ? 'current ' : '' }}">
-                    {{-- <a class="cart-link" href="{{ route('cart') }}">@lang('transFront.cart')
-                        <div class="cart-count"></div>
-                    </a> --}}
-                    {{-- <a href="cart.html" class="main-menu__cart insur-two-icon-shopping-cart"></a> --}}
-                    <div class="">
+                <li class="{{ Route::currentRouteName() == 'cart' ? 'current ' : '' }} cart-link">
+                    {{-- <div class="">
                         <div class="cart-link">
-                            {{-- <a href="#" class="main-menu__search search-toggler icon-magnifying-glass"></a> --}}
                             <a href="{{ route('cart') }}" class="main-menu__cart insur-two-icon-shopping-cart ">
                                 <div class="cart-count"></div>
                             </a>
                         </div>
-                        {{-- <div class="main-menu__main-menu-box-get-quote-btn-box">
-                            <a href="contact.html" class="thm-btn main-menu__main-menu-box-get-quote-btn">Get a Quote</a>
-                        </div> --}}
-                    </div>
+                    </div> --}}
+                    <a href="{{ route('cart') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                        <div class="cart-count"></div>
+                    </a>
+                </li>
+                <li class="{{ in_array(Route::currentRouteName(), ['brands.index']) ? 'current' : '' }}">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                    </a>
+
+                </li>
+                <li class="{{ in_array(Route::currentRouteName(), ['brands.index']) ? 'current' : '' }}">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                    </a>
+
                 </li>
             </ul>
         </div>
